ebe61c88511026ffa034f69be2cd90abf437099d
abuicke <abuicke@treemetrics.com>
2018-05-01 16:12:25 +0100

    removed unused imprt
    removed unused imprt

--- app/src/main/java/ie/moses/recyclergridview/test/MainActivity.kt
+++ app/src/main/java/ie/moses/recyclergridview/test/MainActivity.kt
@@ -3,7 +3,6 @@ package ie.moses.recyclergridview.test
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import ie.moses.recyclergridview.R
 import ie.moses.recyclergridview.core.OnItemClickListener
 import ie.moses.recyclergridview.util.toast
