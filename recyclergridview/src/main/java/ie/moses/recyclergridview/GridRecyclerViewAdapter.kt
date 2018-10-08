5c05e929c4e0c2f8b94961f6ee21d528e546b6cb
abuicke <mosesjayjayjay@gmail.com>
2018-10-08 21:30:03 +0100

    moved DoubleExtensions to util package
    moved DoubleExtensions to util package

--- recyclergridview/src/main/java/ie/moses/recyclergridview/GridRecyclerViewAdapter.kt
+++ recyclergridview/src/main/java/ie/moses/recyclergridview/GridRecyclerViewAdapter.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import ie.moses.cantor.Cantor
+import ie.moses.recyclergridview.util.roundUp
 
 abstract class RecyclerViewAdapter<ViewHolder : RecyclerView.ViewHolder, T : Any>(
         context: Context, protected var data: List<T>, protected var onItemClickListener: OnItemClickListener?)
--- recyclergridview/src/main/java/ie/moses/recyclergridview/util/DoubleExtensions.kt
+++ recyclergridview/src/main/java/ie/moses/recyclergridview/util/DoubleExtensions.kt
@@ -1,3 +1,3 @@
-package ie.moses.recyclergridview
+package ie.moses.recyclergridview.util
 
 internal fun Double.roundUp() = Math.ceil(this).toInt()
